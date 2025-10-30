d17641bd3e45b7502f423a4189d503aeab04f694
Tayfun van Zantvoort <titandanium@gmail.com>
2025-10-30 19:25:02 +0100

    fix update App.tsx to use AuthProvider and ListingGenerator components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { client } from './api/client';
+import { ensureFirebase, observeAuthState } from './firebase/auth';
+
+ensureFirebase();
+
+let latestIdToken: string | null = null;
+
+client.configureAuthTokenProvider(async () => {
+  return latestIdToken;
+});
+
+observeAuthState(({ idToken }) => {
+  latestIdToken = idToken;
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
